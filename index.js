7ae1046ca4dacc50d991141144f85d614348e5f4
isaquefreitasdev <84482119+isaquefreitasdev@users.noreply.github.com>
2023-03-16 16:04:45 -0300

    env

---
+++ index.js
@@ -0,0 +1,36 @@
+//Exercises 1
+// let index =  13;
+// let k = 0
+// let soma = 0
+// while(k < index){
+//     k = k + 1
+//     soma = soma + k
+// }
+// console.log(soma)
+//Exercercio 2
+
+// let termo = prompt("Digite o n");
+// let penultimo =0;
+// let ultimo  = 1;
+// let numero;
+// if(termo<=2){
+//     numero = termo - 1
+// }else{
+//     for(let count = 3; count<=termo; count++){
+//         numero = ultimo + penultimo
+//         penultimo = ultimo;
+//         ultimo = numero;
+//     }
+// }
+// console.log(numero)
+// Exercercio 3
+// a:9
+// b:128
+// c:49
+// d:100
+// e:13
+// f:200
+
+//Exercicio 5
+// let text = "olá mundo"
+// console.log(text.split('').reverse().join(''))
